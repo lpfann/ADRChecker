d0be38c83744d40f999380777bff92b28010fa07
Lukas Pfannschmidt <mirek.1337@gmail.com>
2015-05-15 12:18:36 +0200

    Grundlagen implementiert MaterialDrawer Datenbank access

---
+++ app/src/main/java/unibi/com/medicapp/QueryDatabase.java
@@ -0,0 +1,46 @@
+package unibi.com.medicapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * @author Lukas Pfannschmidt
+ *         Date: 13.05.2015
+ *         Time: 11:58
+ */
+public class QueryDatabase extends SQLiteAssetHelper {
+    private static final String DATABASE_NAME = "converted.sqlite";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String BEMERKUNGEN = "p450_bemerkungen";
+    private static final String INTERATKIONEN = "p450_interaktionen";
+    private static final String ISOENZYME = "p450_isoenzyme";
+    private static final String LITERATUR = "p450_literaturverzeichnis";
+    private static final String METABOLISMUS = "p450_metabolismus";
+    private static final String SUBSTANZEN = "p450_substanz";
+    private static final String SUBSTANZ_WIRKSTOFF_MAPPING = "p450_substanz_wirkstoff_mapping";
+    private static final String THERAPEUTISCHE_KLASSIFIKATION = "p450_therapeutische_klassifikation";
+
+    public QueryDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    public Cursor getSubstances() {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        String[] sqlSelect = {"0 _id", "name"};
+
+        qb.setTables(SUBSTANZEN);
+        Cursor c = qb.query(db, sqlSelect, null, null,
+                null, null, null);
+
+        c.moveToFirst();
+        return c;
+
+    }
+}
--- app/src/main/java/unibi/com/medicapp/SearchActivity.java
+++ app/src/main/java/unibi/com/medicapp/SearchActivity.java
@@ -1,24 +1,80 @@
 package unibi.com.medicapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.SimpleCursorAdapter;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 
-public class SearchActivity extends ActionBarActivity {
+public class SearchActivity extends AppCompatActivity {
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    private QueryDatabase db;
+    private Drawer.Result result = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.inject(this);
+        setSupportActionBar(toolbar);
+
+        AccountHeader.Result headerResult = new AccountHeader()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .build();
+
+
+        result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withTranslucentStatusBar(true)
+                .withActionBarDrawerToggle(true)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home),
+                        new PrimaryDrawerItem().withName(getString(R.string.enzyme_interaction)).withIcon(FontAwesome.Icon.faw_exchange),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
+                        new SecondaryDrawerItem().withName(getString(R.string.info)).withIcon(FontAwesome.Icon.faw_info)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                        //TODO Menu Switcher Logik
+                    }
+
+                })
+                .build();
+
+
+        db = new QueryDatabase(this);
+        Cursor substances = db.getSubstances();
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.wirkstoffAutoComplete1);
+        SimpleCursorAdapter mCursor;
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
+        //getMenuInflater().inflate(R.menu.menu_search, menu);
         return true;
     }
 
